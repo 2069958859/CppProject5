9d1167cb88ca9e58d7b6c07af3b0169dca9f0527
Libing <12110141@mail.sustech.edu.cn>
2022-12-15 08:44:48 +0800

    2

--- source.h
+++ source.h
@@ -13,25 +13,64 @@ using namespace std;
 template <typename T>
 class Matrix
 {
+    int *ref_count = new int(0);
+    T *data = nullptr;
     size_t row;
     size_t column;
     size_t channels;
 
-    // T *data;
-    // template <typename T>
-    // class shared_ptr;
-    std::shared_ptr<T> *data = nullptr;
-
 public:
     Matrix();
-    Matrix(size_t row, size_t col) : row(row), column(col)
+    Matrix(size_t row, size_t col, size_t channels) : row(row), column(col), channels(channels)
     {
-        std::shared_ptr<T> *data = new shared_ptr<T>[row * col * channels * sizeof(T)] {};
+        try
+        {
+            if (row <= 0 || col <= 0 || channels <= 0)
+            {
+                throw "The matrix is not valid!";
+            }
+            else
+            {
+                *ref_count = 1;
+                data = new T[row * col * channels]{};
+            }
+        }
+        catch (const bad_alloc &e)
+        {
+            cerr << "Memory is fail to allocate" << endl;
+            delete ref_count;
+            delete[] data;
+            data = nullptr;
+            throw "Memory is fail to allocate";
+        }
+        catch (const char *msg)
+        {
+            cerr << msg << endl;
+            delete ref_count;
+            delete[] data;
+            data = nullptr;
+            ref_count = nullptr;
+            throw msg;
+        }
+    }
+    void addrefCount()
+    {
+        this->ref_count++;
+    }
+    void minusrefCount()
+    {
+        this->ref_count--;
+    }
+    int *getrefCount() const // const很重要
+    {
+        return this->ref_count;
     }
     Matrix operator+(const Matrix &mat);
     Matrix operator-(const Matrix &mat);
     Matrix operator=(const Matrix &mat);
     bool operator==(const Matrix &mat);
+    Matrix operator+(const T addx);
+    Matrix operator*(const Matrix &mat);
     friend std::ostream &operator<<(std::ostream &os, const Matrix &mat)
     {
         if (mat.data == NULL)
@@ -60,10 +99,9 @@ public:
         return os;
     }
 
-    Matrix operator+(const T addx);
-
     void initialMatrix(Matrix *matrix, const size_t row, const size_t col);
     void createRamMatrix(Matrix *matrix, const size_t row, const size_t col, const size_t databound);
     // void showMatrix(const Matrix *matrix);
 };
+
 #endif // UNTITLED_SOURCE_HPP
